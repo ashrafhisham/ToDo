45884478fcaf5f867cdc025fcdacab67f892c267
ashraf <ashraf@192.168.31.128>
2019-01-08 20:48:17 +0200

    Local Data Persisted Using UserDefaults

--- ToDo/TodoListViewController.swift
+++ ToDo/TodoListViewController.swift
@@ -11,9 +11,16 @@ import UIKit
 class TodoListViewController: UITableViewController{
     
     var itemArray = ["ashraf","mohamed","hisham"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+            
+            itemArray = items
+        }
 
     }
     
@@ -61,6 +68,8 @@ class TodoListViewController: UITableViewController{
             //what will happen when press add button.
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
             
